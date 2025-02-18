051b2f5a665f6e973c433dd890baf1548aa2dcef
thallysfs <thallys@hotmail.com>
2025-02-18 09:08:28 -0300

    fix: adjusted register page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -4,16 +4,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Registration = async ({ params: {userId}}: SearchParamProps) => {
-  console.log("🚀 ~ Registration ~ userId:", userId)
+const Registration = async ({params}: SearchParamProps) => {
+  const { userId } = await params
   const user = await getUser(userId) 
+  console.log("🚀 ~ Registration ~ userId:", userId)
 
 
 return (
   <div className="flex h-screen max-h-screen">
       {/* TODO: OTP verification | PassKeyModal */}
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image 
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -24,14 +25,9 @@ return (
 
           <RegisterForm user={user} />
 
-          <div className='text-14-regular mt-20 flex justify-between'>
-            <p className='justify-items-end text-dark-600 xl:text-left'>
-              © 2025 CarePulse
-            </p>
-            <Link href="/?admin=true" className='text-gray-500'>
-              Admin
-            </Link>
-          </div>
+          <p className='justify-items-end text-dark-600 xl:text-left'>
+            © 2025 CarePulse
+          </p>
         </div>
       </section>
 
