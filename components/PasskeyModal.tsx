c071ff0f999865a150951653fefc22cad9e711ed
thallysfs <thallys@hotmail.com>
2025-02-24 22:17:58 -0300

    feat: add otp key

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -11,10 +11,48 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { encryptKey } from "@/lib/utils";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { set } from "zod";
 
 const PasskeyModal = () => {
+  const router = useRouter();
   const [open, setOpen] = useState(true);
+  const [passKey, setPassKey] = useState("");
+  const [error, setError] = useState("");
+
+  const encryptedKey =
+    typeof window !== "undefined" ? localStorage.getItem("accessKey") : null;
+
+  const validatePassKey = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    if (passKey === process.env.NEXT_PUBLIC_ADMIN_PASSKEY) {
+      const encryptedKey = encryptKey(passKey);
+
+      localStorage.setItem("accessKey", encryptedKey);
+
+      setOpen(false);
+    } else {
+      setError("Chave de acesso inválida");
+    }
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+    router.push("/");
+  };
 
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
@@ -22,15 +60,51 @@ const PasskeyModal = () => {
         <AlertDialogHeader>
           <AlertDialogTitle className="flex items-start justify-between">
             Admin - Verificação de acesso
+            <Image
+              src="/assets/icons/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => closeModal()}
+              className="cursor-pointer"
+            />
           </AlertDialogTitle>
           <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Para acessar a página administrativa, por favor entre com a chave de
+            acesso.
           </AlertDialogDescription>
         </AlertDialogHeader>
+
+        <div>
+          <InputOTP
+            maxLength={6}
+            value={passKey}
+            onChange={(value) => setPassKey(value)}
+          >
+            <InputOTPGroup className="shad-otp">
+              <InputOTPSlot className="shad-otp-slot" index={0} />
+              <InputOTPSlot className="shad-otp-slot" index={1} />
+              <InputOTPSlot className="shad-otp-slot" index={2} />
+              <InputOTPSlot className="shad-otp-slot" index={3} />
+              <InputOTPSlot className="shad-otp-slot" index={4} />
+              <InputOTPSlot className="shad-otp-slot" index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+
+          {error && (
+            <p className="shad-error text-14-regular mt-4 flex justify-center">
+              {error}
+            </p>
+          )}
+        </div>
+
         <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction
+            onClick={(e) => validatePassKey(e)}
+            className="shad-primary-btn w-full"
+          >
+            Entre com o código
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
